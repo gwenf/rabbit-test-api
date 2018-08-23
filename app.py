161935d49f5ab35e5fc6e1b93dd488f34bf6b5b1
Gwen Faraday <gwenfaraday@gmail.com>
2018-08-23 12:32:02 -0400

    updating flask for heroku

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello world"
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
+
